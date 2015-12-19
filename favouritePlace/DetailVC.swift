44c52ec20bf6eac03fdc2689bfa22b71b4b00d60
andrewchng <andrewchng92@gmail.com>
2015-12-19 13:01:52 +0800

    first commit

--- favouritePlace/DetailVC.swift
+++ favouritePlace/DetailVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailVC.swift
 //  favouritePlace
 //
 //  Created by Andrew Chng on 19/12/15.
@@ -8,22 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailVC: UIViewController {
 
-    @IBOutlet weak var moreBtn: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        moreBtn.layer.cornerRadius = 2.0
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    @IBAction func onBackPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 
 }
-
